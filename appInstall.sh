562ce23614227a17a2c6e3cb2933739ee156375f
bkp515 <48100515+bkp515@users.noreply.github.com>
2020-02-19 15:37:30 -0700

    Create appInstall.sh

---
+++ appInstall.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#photo editing software
+apt install gimp -y
+apt install inkscape -y
+apt install pinta -y
+apt install digikam -y
+apt install krita -y
+apt install darktable -y
+apt install rawtherapee -y
+apt install openshot -y
+#Video editing software
