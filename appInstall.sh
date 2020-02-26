43801d82e9b4c55ab420b32c4b8e71c5a8fe2137
bkp515 <48100515+bkp515@users.noreply.github.com>
2020-02-26 15:20:10 -0700

    Nearly done

--- appInstall.sh
+++ appInstall.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
+#Repo Install
+#KDEnlive
+add-apt-repository ppa:kdenlive/kdenlive-stable
+#Avidemux
+sudo add-apt-repository ppa:ubuntuhandbook1/avidemux
+#Open Shot
+sudo add-apt-repository ppa:openshot.developers/ppa
+
+#Update
+apt update
+
 #photo editing software
-apt install gimp -y
-apt install inkscape -y
-apt install pinta -y
-apt install digikam -y
-apt install krita -y
-apt install darktable -y
-apt install rawtherapee -y
-apt install openshot -y
+apt install gimp openshot inkscape pinta digikam krita darktable rawtherapee -y
 #Video editing software
+apt install pitivi kdenlive frei0r-plugins avidemux2.7-qt5 avidemux2.7-qt5-data avidemux2.7-plugins-qt5 avidemux2.7-jobs-qt5 openshot-qt -y
